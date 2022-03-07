dcccd6af5c085e4f6eef6cb21433d0d52b4c0a40
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-03-06 22:18:33 -0500

    JS update 3/6/22 #1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const canvas = document.gerElementById('canvas1');
+const ctx = canvas.getContext('2d');
+// console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// ctx.fillStyle = 'white';
+// ctx.fillRect(10, 20, 150, 50);
+
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'red';
+ctx.lineWidth - 5;
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, Math.PI * 2);
+ctx.strokeStyle();
+ctx.fill();
+console.log(ctx);
